74bab10f022a15e4dabf03748d5aadff09cd50a1
lcorrea <yancel209@gmail.com>
2022-11-06 18:08:12 -0500

    agregando información de microservicio de usuarios

---
+++ README.md
@@ -0,0 +1,4 @@
+# microservicio de usuarios
+
+# curso
+https://www.udemy.com/course/microservicios-con-spring-boot-y-spring-cloud/
---
+++ src/main/resources/import.sql
@@ -0,0 +1,33 @@
+--INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+--INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+
+--INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+--INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+
+--INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
+--INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 2);
+--INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2, 1);
+
+
+--INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+--INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+
+--INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+--INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+--INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1, 1);
+--INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 2);
+--INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2, 1);
+
+select
+*
+from Usuarios
+
+--$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86
+--$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK
+--$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK
+
+--$2a$10$e6yN1D9bUK/xa3Er0FbYZO2IYogRIcM3qQjAQNu.dp9H0n0TX5J9S
+--$2a$10$PxoNlejHAvX9wKi2KiGfde8mrPXJKz0LGmEzqQqSzCALFiSlKFW9y
+--$2a$10$kaRwJAcbY0IohE5Et.kMeeU.wV1GPUTrufGwfte4apu9nCuwXOuLO
+--$2a$10$tUDYrvjhqnDBPNhaZFW8qOFLDlP4AFIu/hlMpM0dJ7PHeq0sWP0WK
